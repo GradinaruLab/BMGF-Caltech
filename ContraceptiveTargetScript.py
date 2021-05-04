0e4db63ad707a78347985da9a9cfb206ccf2e6b7
csaada <crsaada@gmail.com>
2021-05-04 10:23:04 -0700

    added documentation and commented out some debug code

--- ContraceptiveTargetScript.py
+++ ContraceptiveTargetScript.py
@@ -13,19 +13,53 @@ import csv
 
 
 def compare_targets():
+    """
+    Given a csv from Open Targets, generates a list containing one dictionary
+    for each target. The dictionary keys are provided in 
+    ContraceptiveConstants.py. The information in the dictionary is:
+        -the ensembl ID
+        -the gene name
+        -the overall association score from Open Targets
+        -the RNA tissue data from the Human Protein Atlas
+
+    Returns
+    -------
+    target_list : List
+    
+        A list of dictionaries containing the RNA tissue data from 
+        the Human Protein Atlas; in addition the gene name, 
+        ensembl ID, and overall association score from Open Targets
+
+    """
     target_list = ota.data_from_csv(cc.CSV_NAME)
-    counter = 0
+    #counter = 0 #debugging
     for target_dict in target_list:
         target_ID = target_dict[cc.ENSID_KEY]
         paResponse = paa.get_protein_xml(target_ID)
         norm_RNA_exp = paa.get_RNA_tissue_data(paResponse)
         target_dict[cc.RNA_EXP_KEY] = norm_RNA_exp
-        print(counter)
-        print(target_dict[cc.GN_KEY])
-        counter += 1
+        #print(counter) #so I can see that it's actually running
+        #print(target_dict[cc.GN_KEY])
+        #counter += 1 #debugging
     return target_list
 
 def write_target_csv(target_list):
+    """
+    Writes the list of target dictionaries to a csv file.
+    
+    Currently, the file is named "target_list_[today's date]"
+    #TODO make this a parameter in constants
+
+    Parameters
+    ----------
+    target_list : List
+        The list of dictionaries provided by compare_targets
+
+    Returns
+    -------
+    None.
+
+    """
     today = date.today()
     today_str = today.strftime("%b-%d-%Y")
     out_csv_name = "target_list_" + today_str + ".csv"
@@ -40,5 +74,14 @@ def write_target_csv(target_list):
             
     
 def script_wrapper():
+    """
+    Wrapper function that runs the script. Generates the target list and
+    writes it to a csv file. 
+
+    Returns
+    -------
+    None.
+
+    """
     target_list = compare_targets()
     write_target_csv(target_list)
