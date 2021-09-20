5f99b64f9eb2f3e3ad8d10283a9f5c17ddbec086
csaada <49408571+csaada@users.noreply.github.com>
2021-09-20 15:22:17 -0700

    wrote functions to get mouse gene expression data in the ovary as counts of expression strength level. Integrated ContraceptiveConstants into MouseMineAccess.

--- ContraceptiveConstants.py
+++ ContraceptiveConstants.py
@@ -4,25 +4,6 @@ Created on Fri Mar 12 10:16:59 2021
 
 @author: Carrie
 """
-TEST_CSV = "merged_test.csv" 
-CSV_NAME = "merged_OT_and_CITD_targets.csv"
-#default name for the csv downloaded from opentargets
-OLD_OT_CSV = "targets_associated_with_female_infertility.csv"
-OT_CSV_NAME = "EFO_0008560-associated-diseases.csv" #the updated version
-#CITDBase
-TEST_CITD_CSV = "CITDBase_test.csv"
-CITD_CSV_NAME = "CITDBase-Target-Search.csv"
-
-TEST_ID = "ENSG00000178394" #ID for the protein HTR1A
-#this protein is expressed in the ovaries, with both protein and RNA evidence
-TEST_NEG_ID = "ENSG00000173714" #WFIKKN2 which has no expression data in any
-#ovarian tissue
-TEST_PART_ID = "ENSG00000163093" #BBS5 which has expression data in stroma
-#but not in follicles
-TEST_FALSE_POS_ID = "ENSG00000110888" #CAPRIN2 which has a high staining
-#expression, but not detected actual expression, expert annotated staining
-#as off-target binding.
-
 
 #keys for the dictionaries in the list of targets
 ENSID_KEY = "ensembl ID"
@@ -40,9 +21,15 @@ OVARY_TYPES = [STROMA, FOLLICLE]
 
 OVARY_ONT_ID = "UBERON:0000992"
 
+MGI_EXP = 'GXDExpression.strength'
+
 #Names for databases in the dictionaries
 OT = "Open Targets"
 CITD = "CITDBase"
+MGI = "MGI_ID"
 
 #expression not available
 EXP_NA = "na"
+
+#csv encoding
+ENC = 'utf-8-sig'
